9086aa4a8f6fdac3c0a6d98790caf69cd97dad10
happycola008 <138659004+happycola008@users.noreply.github.com>
2024-01-05 13:30:55 +0800

    Update C_AR02.c

--- C_AR02.c
+++ C_AR02.c
@@ -2,14 +2,9 @@
 
 int main() {
     int arr[6];
-    
-
-    // 讀取陣列元素
     for (int i = 0 ; i<6 ; i++) {
         scanf("%d", &arr[i]);
     }
-
-    // 印出翻轉陣列
     for (int i = 5; i > 0; i--) {
         printf("%d ", arr[i]);
     }
